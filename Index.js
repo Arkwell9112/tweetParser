e82dfcfaad885c399936c8c9389fe15e58264e03
Florian BOURRIER <florianbourrier@gmail.com>
2020-12-09 17:17:35 +0100

    add graph and export

--- Index.js
+++ Index.js
@@ -6,6 +6,8 @@ let commandParser = require("./CommandParser/CommandParser");
 let cSVReader = require("./CSVReader/CSVReader");
 let topTweets = require("./TopTweets/TopTweets");
 let searchTweets = require("./SearchTweets/SearchTweets");
+const graphics = require('./Graphics/Graphics');
+const exportTweet = require('./ABNF/ABNFTweet');
 
 function getRelatedHashtagsWrapper(commandParts) {
     let hashtagList = Array();
@@ -38,11 +40,39 @@ function searchTweetsWrapper(commandParts){
 	return searchTweets.searchByCriteria(data, criterias);
 }
 
+
+function listGraphWrapper(){
+    graphics.listGraph(data,commandParts[0],commandParts[1]);
+    return "managed";
+}
+
+function exportTweetWrapper(){
+    if (data === null) {
+        console.log("Data is null")
+        return "managed";
+    } else if (data.hasOwnProperty("length")) {
+        if (data.length === 0) {
+            console.log("Data is null")
+            return "managed";
+        } else {
+            if (data[0].hasOwnProperty("hashtags")) {
+                exportTweet.exportTweetABNF(data);
+                return "managed";
+            } else {
+                console.log("Data is not a TweetList");
+                return "managed";
+            }
+        }
+    }
+}
+
 commandParser.addCommand("import", 1, "Import data in the application from CSV TweetList.", "import <String> path", parseGL02TweetsWrapper);
 commandParser.addCommand("exit", 0, "Exit the application.", "exit", exitWrapper);
 commandParser.addCommand("print", 0, "Print current data.", "print", printWrapper);
 commandParser.addCommand("relhashtags", 2, "Get related hashtags from hashtag depending on level.", "relhashtags <String> hashtag <Integer> level", getRelatedHashtagsWrapper);
 commandParser.addCommand("search", 1, "Search tweets matching given criterias", "search [{<String> criteriaName, <String/Integer/Float> value, <String> operand, <String, function js code> fncOperand }]", searchTweetsWrapper);
+commandParser.addCommand("listGraph",2,"Export graphic from author list or tweet list.\nFirst parameter can be : created_at, user_screen_name, user_name, hashtags\nSecond can be : count, retweet_count","listGraph <String> xValue <String> yValue",listGraphWrapper);
+commandParser.addCommand("exportTweet",0,"Export list of tweet from a tweet list","exportTweet",exportTweetWrapper);
 
 let data = null;
 let dataType = -1;
